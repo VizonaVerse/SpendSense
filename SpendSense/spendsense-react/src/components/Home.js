0abd1f02af8c100c120623a6de2e8beda5ce3cf9
xinru <xw00892@surrey.ac.uk>
2025-03-24 15:12:56 +0000

    NES.CSS pixelated font

--- SpendSense/spendsense-react/src/components/Home.js
+++ SpendSense/spendsense-react/src/components/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
+import "nes.css/css/nes.min.css";
+import "../App.css"; 
 
 function Home({ onStart }) {
   return (
-    <div className="section-content home-section text-center">
+    <div className="section-content home-section text-center is-pixelated">
       <h1>Welcome to SpendSense</h1>
       <button onClick={onStart} className="btn btn-primary btn-lg">
         Start Game

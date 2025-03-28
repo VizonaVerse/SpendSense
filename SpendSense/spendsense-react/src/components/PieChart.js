b0e1fe34d882bf33458fa79ee8e69e7eee02b446
Dominic Soto <ds01663@surrey.ac.uk>
2025-03-28 14:33:25 +0000

    Change Pie Chart to 3 segments, "Wants, needs and Savings"

--- SpendSense/spendsense-react/src/components/PieChart.js
+++ SpendSense/spendsense-react/src/components/PieChart.js
@@ -12,17 +12,15 @@ const PieChart = () => {
   const dragTimeout = useRef(null);
 
   const [chartData, setChartData] = useState({
-    labels: ['Rent', 'Food', 'Transport', 'Utilities', 'Entertainment'],
+    labels: ['Wants', 'Needs', 'Savings' ],
     datasets: [
       {
         label: 'Spending',
-        data: [30, 20, 15, 10, 25],
+        data: [30, 20, 50],
         backgroundColor: [
           'rgba(255, 99, 132, 0.6)',
           'rgba(54, 162, 235, 0.6)',
           'rgba(255, 206, 86, 0.6)',
-          'rgba(75, 192, 192, 0.6)',
-          'rgba(153, 102, 255, 0.6)',
         ],
         hoverOffset: 20,
       },
